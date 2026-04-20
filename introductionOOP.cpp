67d597f8210308b141f7a9a5c76725ce291cd0fd
Moreno <nongfbndr@gmail.com>
2026-04-20 10:51:17 +0700

    tambah function printdata

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,7 +8,8 @@ public:
     string nama;
     float nilai;
 
-    void printData(){
+    void printData()
+    {
         cout << "NIM : " << nim << endl;
         cout << "Nama : " << nama << endl;
         cout << "Nilai : " << nilai << endl;
